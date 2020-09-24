e6dca1d9763020b2cb684e619c2935d74e194b6f
dmholland <kenoshatrader@gmail.com>
2020-09-24 11:39:08 -0400

    I had to add @Controller Annotation to my registration controller so that thymeleaf would recognize the models in the tags

--- src/main/java/com/dmholland/demo/controllers/RegistrationController.java
+++ src/main/java/com/dmholland/demo/controllers/RegistrationController.java
@@ -3,8 +3,9 @@ package com.dmholland.demo.controllers;
 
 import com.dmholland.demo.models.User;
 import com.dmholland.demo.services.NotificationService;
-import com.dmholland.demo.services.UserServiceRepository;
+import com.dmholland.demo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,32 +16,36 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
 
-
+@Controller
 public class RegistrationController {
     @Autowired
-    private UserServiceRepository repository;
+    private UserService userService;
 
 
-    @GetMapping("/users/registration")
-    public String registrationPage(WebRequest request, Model model){
-   User user=repository.createUser(new User());
-        model.addAttribute("user",user);
-        return "users/registration";
+    @RequestMapping("/users/registration")
+    public ModelAndView register(){
+        ModelAndView mv= new ModelAndView();
+        User user=new User();
+  // User user=repository.createUser(new User());
+        //I had get mapping and used the repository
+        mv.addObject(user);
+        mv.setViewName("users/registration");
+        return mv;
     }
 
     @RequestMapping(value="users/registration",method = RequestMethod.POST)
     public ModelAndView register(@Valid User user, BindingResult validate){
         ModelAndView mv=new ModelAndView();
-        if(repository.userCheck(user)){
+        if(userService.userCheck(user)){//user exists
             validate.rejectValue("userName", "error.user", "User exists");
             mv.setViewName("users/registration");
             return mv;
         }
-        this.repository.createUser(user);
+        this.userService.createUser(user);
         mv.addObject("user",user);
         mv.addObject("successMessage", "User has been created");
         mv.addObject("user", new User());
-        mv.setViewName("users/login");
+        mv.setViewName("users/registration");
         return mv;
 
     }
